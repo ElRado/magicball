a0a7cc53386c4c01fcb4d2705511caea9d4da0d8
Mike Gonzales <mikegonzales21@gmail.com>
2021-06-20 21:55:39 -0700

    added answer list in JS

---
+++ js/index.js
@@ -0,0 +1,4 @@
+const ball = {};
+ball.listofanswers = [
+    "It is certain.", "It is decidedly so.", "Without a doubt.", "Yes, definitely.", "You may rely on it.", "As I see it, yes.", "Ehh, probably.", "lookin good.", "Yes. Now leave.", "Signs point to yes.", "Reply hazy.", "I'll get back to you", "Cant tell you that here.", "whaddaya think I am? Psychic?", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "That's not lookin good.", "Very doubtful."
+];
