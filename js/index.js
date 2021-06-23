1b8ee69efa323a49a10e2f8531df6222f3534213
Mike Gonzales <mikegonzales21@gmail.com>
2021-06-23 08:24:32 -0700

    added stylings to the site, continued to try and get answer to display with no luck

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
-const answers = [
+const answers ={} [
     "It is certain.", "It is decidedly so.", "Without a doubt.", "Yes, definitely.", "Drink Duff", "As I see it, yes.", "Ehh, probably.", "lookin good.", "Yes. Now leave.", "Signs point to yes.", "Reply hazy.", "I'll get back to you", "Cant tell you that here.", "whaddaya think I am? Psychic?", "have a Duff and ask agian", "Don't count on it.", "My reply is no.", "My sources say no.", "That's not lookin good.", "Very doubtful."
 ];
 
 const displayAnswer = function() {
 
 	let index = Math.random(push * answers.length);
-	let answer = answers[answers];
+	let answer = answers[index];
 	let element = document.getElementById( "resolve" );
 	//element.style.display = 'inline-block';
-	element.innerHTML = '<br><br>' + answer;
+	//element.innerHTML = '<br><br>' + answer;
 }
 
 const input = document.getElementById("myText");
@@ -20,6 +20,6 @@ const eight = document.getElementById("resolve");
 	  else {
 		eight.innerText = "";
 		answers.innerText = "myText";}
-    eight.innerText = answer;
+   	eight.innerText = answer[index];
   });
 
