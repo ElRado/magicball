1bcb56d07ff8b79a1eb5f307220077765f6c9abb
Mike Gonzales <mikegonzales21@gmail.com>
2021-06-23 11:39:07 -0700

    fixed error

--- js/index.js
+++ js/index.js
@@ -1,10 +1,9 @@
-const answers ={} [
+const answers = [
     "It is certain.", "It is decidedly so.", "Without a doubt.", "Yes, definitely.", "Drink Duff", "As I see it, yes.", "Ehh, probably.", "lookin good.", "Yes. Now leave.", "Signs point to yes.", "Reply hazy.", "I'll get back to you", "Cant tell you that here.", "whaddaya think I am? Psychic?", "have a Duff and ask agian", "Don't count on it.", "My reply is no.", "My sources say no.", "That's not lookin good.", "Very doubtful."
 ];
 
 const displayAnswer = function() {
-
-	let index = Math.random(push * answers.length);
+	let index = Math.floor(Math.random() * answers.length);
 	let answer = answers[index];
 	let element = document.getElementById( "resolve" );
 	//element.style.display = 'inline-block';
